c1a4899ea4a8ebff03b40568e0d8fb6eadf4ce99
aaditx23 <adit.pranto@gmail.com>
2023-08-28 00:12:43 +0600

    no sync

--- property/settings.py
+++ property/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "property360",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "localhost",
+        "USER": "django",
+        "PASSWORD": "django",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     }
 }
